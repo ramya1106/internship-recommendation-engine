7204b8a02cb4911e88a68f3ebef1a83f65611bba
Amrutha Ponduri <amruthasanthoshi2003@gmail.com>
2025-12-01 00:28:51 +0530

    Corrected class name typo and added @Component annotation to enable Spring bean discovery

--- reccomendation_system/src/main/java/com/example/reccomendation_system/helper/EuclideanDistanceCalculator.java
+++ reccomendation_system/src/main/java/com/example/reccomendation_system/helper/EuclideanDistanceCalculator.java
@@ -1,6 +1,9 @@
 package com.example.reccomendation_system.helper;
 
-public class EucledianDistanceCalculator {
+import org.springframework.stereotype.Component;
+
+@Component
+public class EuclideanDistanceCalculator {
     public double calculateDistance(double x1, double y1, double x2, double y2) {
         return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
     }
